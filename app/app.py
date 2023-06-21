3fb9bd3249623850e06c7ddf149c2572db93a6dc
Ammar <75755548+amxr21@users.noreply.github.com>
2023-06-21 11:56:41 +0400

    update lib

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, Response, send_file
 import glob, os , pdfplumber, datefinder, re
 import pandas as pd
-import os
 import tabula
 import csv
 # import time
