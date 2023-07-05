a170a2b6d4f6d2caaebda2ebde9b6c29efad8b7c
fatima-dakalbab <fatima.dakalbab@gmail.com>
2023-07-05 12:54:05 +0400

    java

--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 # Install Java
 sdk install java
 
+# Install Java Development Kit (JDK)
+sdk install java
+
+# Set Java environment variables
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+export PATH="$JAVA_HOME/bin:$PATH"
+
 # Upgrade pip
 pip install --upgrade pip
 
