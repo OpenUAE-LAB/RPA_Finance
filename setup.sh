9451106219d93ebb3391c0d87344e6e8d882d59f
fatima-dakalbab <fatima.dakalbab@gmail.com>
2023-07-05 15:36:25 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+# Print system environment variables
+echo "JAVA_HOME: $JAVA_HOME"
+echo "PATH: $PATH"
+
 # Install sdkman
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -8,6 +12,10 @@ sdk install java
 # Set Java environment variables
 export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
 export PATH="$JAVA_HOME/bin:$PATH"
+
+# Print updated system environment variables
+echo "JAVA_HOME: $JAVA_HOME"
+echo "PATH: $PATH"
 
 # Upgrade pip
 pip install --upgrade pip
